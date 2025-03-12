e8798aaa6a8e6328a8023f1a4a7b790bac5ac150
Pevin Elsenique <pevin.elsenique@shippit.com>
2025-03-12 14:55:47 +0800

    Fix git named files not appearing in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -60,12 +60,12 @@ return {
       },
       pickers = {
         find_files = {
-          file_ignore_patterns = { 'node_modules', '.git', '.venv' },
+          file_ignore_patterns = { 'node_modules', '.git/', '.venv' },
           hidden = true,
         },
       },
       live_grep = {
-        file_ignore_patterns = { 'node_modules', '.git', '.venv' },
+        file_ignore_patterns = { 'node_modules', '.git/', '.venv' },
         additional_args = function(_)
           return { '--hidden' }
         end,
