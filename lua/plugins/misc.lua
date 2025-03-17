9df562601d6b2753acaa1df941e3843ee6ffd9f3
pevin <elsenique.pevin@gmail.com>
2025-03-17 11:25:19 +0800

    Install vimbegood plugin

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,6 +1,9 @@
 -- Standalone plugins with less than 10 lines of config go here
 return {
   {
+    'ThePrimeagen/vim-be-good',
+  },
+  {
     -- Tmux & split window navigation
     'christoomey/vim-tmux-navigator',
   },
